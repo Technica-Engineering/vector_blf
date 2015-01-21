335b3f2c3d4d60fb58b30403bc3681a9ac3c965b
Tobias Lorenz <tobias.lorenz@gmx.net>
2015-01-22 00:58:08 +0100

    Update unit test with log file.
    Signed-off-by: Tobias Lorenz <tobias.lorenz@gmx.net>

--- src/Vector/BLF/tests/unittests/test_Logfile.cpp
+++ src/Vector/BLF/tests/unittests/test_Logfile.cpp
@@ -21,36 +21,94 @@ BOOST_AUTO_TEST_CASE(Logfile)
     Vector::BLF::File infile;
     Vector::BLF::File outfile;
 
-    /* load database */
-    boost::filesystem::path infile(CMAKE_CURRENT_SOURCE_DIR "/data/logfile.blf");
-    std::string infilename = infile.string();
+    /* input log file */
+    boost::filesystem::path inpath(CMAKE_CURRENT_SOURCE_DIR "/data/logfile.blf");
+    std::string infilename = inpath.string();
     infile.open(infilename);
 
+    /* show file statistics */
+    std::cout << "signature: 0x" << std::hex << infile.fileStatistics.signature << std::endl;
+    BOOST_REQUIRE(infile.fileStatistics.signature == Vector::BLF::FileSignature);
+    std::cout << "applicationId: ";
+    switch(infile.fileStatistics.applicationId) {
+    case Vector::BLF::ApplicationId::UNKNOWN:
+        std::cout << "Unknown";
+        break;
+    case Vector::BLF::ApplicationId::CANALYZER:
+        std::cout << "CANalyzer";
+        break;
+    case Vector::BLF::ApplicationId::CANOE:
+        std::cout << "CANoe";
+        break;
+    case Vector::BLF::ApplicationId::CANSTRESS:
+        std::cout << "CANstress";
+        break;
+    case Vector::BLF::ApplicationId::CANLOG:
+        std::cout << "CANlog";
+        break;
+    case Vector::BLF::ApplicationId::CANAPE:
+        std::cout << "CANape";
+        break;
+    case Vector::BLF::ApplicationId::CANCASEXLLOG:
+        std::cout << "CANcaseXL log";
+        break;
+    default:
+        std::cout << std::dec << (unsigned short) infile.fileStatistics.applicationId;
+        break;
+    };
+    std::cout << std::endl;
+    std::cout << "applicationNumber: " << std::dec << (unsigned short) infile.fileStatistics.applicationMajor << "." << (unsigned short) infile.fileStatistics.applicationMinor << "." << (unsigned short) infile.fileStatistics.applicationBuild << std::endl;
+    std::cout << "apiNumber: " << std::dec << (unsigned short) infile.fileStatistics.apiMajor << "." << (unsigned short) infile.fileStatistics.apiMinor << "." << (unsigned short) infile.fileStatistics.apiBuild << "." << (unsigned short) infile.fileStatistics.apiPatch << std::endl;
+    std::cout << "fileSize: " << std::dec << infile.fileStatistics.fileSize << " = 0x" << std::hex << infile.fileStatistics.fileSize << std::endl;
+    std::cout << "uncompressedFileSize: " << std::dec << infile.fileStatistics.uncompressedFileSize << " = 0x" << std::hex << infile.fileStatistics.uncompressedFileSize << std::endl;
+    std::cout << "objectCount: " << std::dec << infile.fileStatistics.objectCount << std::endl;
+    std::cout << "objectsRead: " << std::dec << infile.fileStatistics.objectsRead << std::endl;
+    //std::cout << "measurementStartTime: " << std::dec << infile.fileStatistics.measurementStartTime << std::endl;
+    //std::cout << "lastObjectTime: " << std::dec << infile.fileStatistics.lastObjectTime << std::endl;
+
+#ifdef OUTFILE_EXISTS
     /* create output directory */
     boost::filesystem::path outdir(CMAKE_CURRENT_BINARY_DIR "/data/");
     if (!exists(outdir)) {
         BOOST_REQUIRE(create_directory(outdir));
     }
 
-    /* save database */
-    boost::filesystem::path outfile(CMAKE_CURRENT_BINARY_DIR "/data/logfile.blf");
-    std::string outfilename = outfile.string();
+    /* output log file */
+    boost::filesystem::path outpath(CMAKE_CURRENT_BINARY_DIR "/data/logfile.blf");
+    std::string outfilename = outpath.string();
     outfile.open(outfilename);
+#endif
 
     /* go through all records */
+#ifdef OUTFILE_EXISTS
     outfile.fileStatistics = infile.fileStatistics;
+#endif
     while(!infile.eof()) {
-        Vector::BLF::ObjectHeaderBase * obj = file.read();
+        Vector::BLF::ObjectHeaderBase * obj = infile.read();
+        BOOST_WARN(obj != nullptr);
         if (obj == nullptr)
             continue;
+        std::cout << "object: coc=" << std::dec << infile.currentObjectCount << " ucfs=" << infile.currentUncompressedFileSize << " ot=" << (unsigned short) obj->objectType << std::endl;
 
+#ifdef OUTFILE_EXISTS
         outfile.write(obj);
+#endif
+
+        delete obj;
     }
 
-    /* loaded and saved file should be equivalent */
-    std::ifstream ifs1(infile.c_str());
-    std::ifstream ifs2(outfile.c_str());
+    /* show file statistics */
+    std::cout << "currentUncompressedFileSize: " << std::dec << infile.currentUncompressedFileSize << " = 0x" << std::hex << infile.currentUncompressedFileSize << std::endl;
+    std::cout << "currentObjectCount: " << std::dec << infile.currentObjectCount << " = 0x" << std::hex << infile.currentObjectCount << std::endl;
+    BOOST_CHECK(infile.fileStatistics.uncompressedFileSize == infile.currentUncompressedFileSize);
+    BOOST_CHECK(infile.fileStatistics.objectCount == infile.currentObjectCount);
+
+    /* input and output log file should be equivalent */
+#ifdef OUTFILE_EXISTS
+    std::ifstream ifs1(inpath.c_str());
+    std::ifstream ifs2(outpath.c_str());
     std::istream_iterator<char> b1(ifs1), e1;
     std::istream_iterator<char> b2(ifs2), e2;
     BOOST_CHECK_EQUAL_COLLECTIONS(b1, e1, b2, e2);
+#endif
 }
