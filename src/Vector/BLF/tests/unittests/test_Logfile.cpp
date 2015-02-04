92438f09cc6549c22f99d56ad4efe692cac3812c
Tobias Lorenz <tobias.lorenz@gmx.net>
2015-02-05 00:40:22 +0100

    Small enhancement to test_Logfile.cpp
    Signed-off-by: Tobias Lorenz <tobias.lorenz@gmx.net>

--- src/Vector/BLF/tests/unittests/test_Logfile.cpp
+++ src/Vector/BLF/tests/unittests/test_Logfile.cpp
@@ -25,11 +25,14 @@ BOOST_AUTO_TEST_CASE(Logfile)
     boost::filesystem::path inpath(CMAKE_CURRENT_SOURCE_DIR "/data/logfile.blf");
     std::string infilename = inpath.string();
     infile.open(infilename);
+    BOOST_REQUIRE(infile.is_open());
 
+    /* FileStatistics */
+    BOOST_REQUIRE(infile.fileStatistics.signature == Vector::BLF::FileSignature);
+#define SHOW_FILE_STATISTICS
 #ifdef SHOW_FILE_STATISTICS
     /* show file statistics */
     std::cout << "signature: 0x" << std::hex << infile.fileStatistics.signature << std::endl;
-    BOOST_REQUIRE(infile.fileStatistics.signature == Vector::BLF::FileSignature);
     std::cout << "applicationName: " << infile.fileStatistics.applicationName() << std::endl;
     std::cout << "applicationNumber: " << std::dec << (unsigned short) infile.fileStatistics.applicationMajor << "." << (unsigned short) infile.fileStatistics.applicationMinor << "." << (unsigned short) infile.fileStatistics.applicationBuild << std::endl;
     std::cout << "apiNumber: " << std::dec << (unsigned short) infile.fileStatistics.apiMajor << "." << (unsigned short) infile.fileStatistics.apiMinor << "." << (unsigned short) infile.fileStatistics.apiBuild << "." << (unsigned short) infile.fileStatistics.apiPatch << std::endl;
@@ -37,8 +40,12 @@ BOOST_AUTO_TEST_CASE(Logfile)
     std::cout << "uncompressedFileSize: " << std::dec << infile.fileStatistics.uncompressedFileSize << " = 0x" << std::hex << infile.fileStatistics.uncompressedFileSize << std::endl;
     std::cout << "objectCount: " << std::dec << infile.fileStatistics.objectCount << std::endl;
     std::cout << "objectsRead: " << std::dec << infile.fileStatistics.objectsRead << std::endl;
-    //std::cout << "measurementStartTime: " << std::dec << infile.fileStatistics.measurementStartTime << std::endl;
-    //std::cout << "lastObjectTime: " << std::dec << infile.fileStatistics.lastObjectTime << std::endl;
+    std::cout << "measurementStartTime: " << std::dec
+              << infile.fileStatistics.measurementStartTime.year << "-" << infile.fileStatistics.measurementStartTime.month << "-" << infile.fileStatistics.measurementStartTime.day << " (" << infile.fileStatistics.measurementStartTime.dayOfWeek << ") "
+              << infile.fileStatistics.measurementStartTime.hour << ":" << infile.fileStatistics.measurementStartTime.minute << ":" << infile.fileStatistics.measurementStartTime.second << "+" << infile.fileStatistics.measurementStartTime.milliseconds << "ms" << std::endl;
+    std::cout << "lastObjectTime: " << std::dec
+              << infile.fileStatistics.lastObjectTime.year << "-" << infile.fileStatistics.lastObjectTime.month << "-" << infile.fileStatistics.lastObjectTime.day << " (" << infile.fileStatistics.lastObjectTime.dayOfWeek << ") "
+              << infile.fileStatistics.lastObjectTime.hour << ":" << infile.fileStatistics.lastObjectTime.minute << ":" << infile.fileStatistics.lastObjectTime.second << "+" << infile.fileStatistics.lastObjectTime.milliseconds << "ms" << std::endl;
 #endif
 
 #ifdef OUTFILE_EXISTS
@@ -58,17 +65,17 @@ BOOST_AUTO_TEST_CASE(Logfile)
 
     /* go through all records */
     while(!infile.eof()) {
-        Vector::BLF::ObjectHeaderBase * obj = infile.read();
-        if (obj == nullptr) {
+        Vector::BLF::ObjectHeaderBase * ohb = infile.read();
+        if (ohb == nullptr) {
             std::cerr << "obj == nullptr at currentObjectCount=" << std::dec << infile.currentObjectCount << std::endl;
             continue;
         }
 
 #ifdef OUTFILE_EXISTS
-        outfile.write(obj);
+        outfile.write(ohb);
 #endif
 
-        delete obj;
+        delete ohb;
     }
 
     /* show file statistics */
