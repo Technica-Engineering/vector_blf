418f9501b342777be4de6ffb0e988822fe9f3257
Tobias Lorenz <tobias.lorenz@gmx.net>
2016-01-07 02:00:00 +0100

    LinMessage2 completed.
    Signed-off-by: Tobias Lorenz <tobias.lorenz@gmx.net>

--- src/Vector/BLF/tests/unittests/test_Events.cpp
+++ src/Vector/BLF/tests/unittests/test_Events.cpp
@@ -1095,7 +1095,27 @@ BOOST_AUTO_TEST_CASE(LinMessage2)
     BOOST_REQUIRE(ohb != nullptr);
     BOOST_REQUIRE(ohb->objectType == Vector::BLF::ObjectType::LIN_MESSAGE2);
     linMessage2 = static_cast<Vector::BLF::LinMessage2 *>(ohb);
-    // @todo test implementation
+    BOOST_CHECK(linMessage2->data[0] == 0x00);
+    BOOST_CHECK(linMessage2->data[1] == 0xf0);
+    BOOST_CHECK(linMessage2->data[2] == 0xf0);
+    BOOST_CHECK(linMessage2->data[3] == 0xff);
+    BOOST_CHECK(linMessage2->data[4] == 0xff);
+    BOOST_CHECK(linMessage2->data[5] == 0xff);
+    BOOST_CHECK(linMessage2->data[6] == 0xff);
+    BOOST_CHECK(linMessage2->data[7] == 0xff);
+    BOOST_CHECK(linMessage2->crc == 0x70);
+    BOOST_CHECK(linMessage2->dir == 1); // Tx
+    BOOST_CHECK(linMessage2->simulated == 1);
+    BOOST_CHECK(linMessage2->isEtf == 0);
+    BOOST_CHECK(linMessage2->etfAssocIndex == 0);
+    BOOST_CHECK(linMessage2->etfAssocEtfId == 0xff);
+    BOOST_CHECK(linMessage2->fsmId == 0xff);
+    BOOST_CHECK(linMessage2->fsmState == 0xff);
+    // reserved
+    BOOST_CHECK(linMessage2->respBaudrate == 19231);
+    BOOST_CHECK(isEqual(linMessage2->exactHeaderBaudrate, 19230.769231));
+    BOOST_CHECK(linMessage2->earlyStopbitOffset == 26000);
+    BOOST_CHECK(linMessage2->earlyStopbitOffsetResponse == 26000);
     delete ohb;
 
     BOOST_CHECK(file.eof());
