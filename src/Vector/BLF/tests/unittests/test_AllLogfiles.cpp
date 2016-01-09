32cb635322e753297f7d34ac7212fc86a2fecfdc
Tobias Lorenz <tobias.lorenz@gmx.net>
2016-01-09 15:16:05 +0100

    Change tests Logfile->AllLofgiles. Add AllEvents. As in Vector::ASC.
    Signed-off-by: Tobias Lorenz <tobias.lorenz@gmx.net>

--- src/Vector/BLF/tests/unittests/test_AllLogfiles.cpp
+++ src/Vector/BLF/tests/unittests/test_AllLogfiles.cpp
@@ -1,4 +1,4 @@
-#define BOOST_TEST_MODULE AllEvents
+#define BOOST_TEST_MODULE AllLogfiles
 #if !defined(WIN32)
 #define BOOST_TEST_DYN_LINK
 #endif
@@ -6,39 +6,43 @@
 #include <boost/filesystem.hpp>
 
 #include <fstream>
+#include <iostream>
 #include <iterator>
 #include <string>
 
 #include "Vector/BLF.h"
 
-BOOST_AUTO_TEST_CASE(AllEvents)
+BOOST_AUTO_TEST_CASE(AllLogfiles)
 {
     /* input directory */
-    boost::filesystem::path indir(CMAKE_CURRENT_SOURCE_DIR "/events/");
+    boost::filesystem::path indir(CMAKE_CURRENT_SOURCE_DIR "/data/");
 
     /* output directory */
-    boost::filesystem::path outdir(CMAKE_CURRENT_BINARY_DIR "/events/");
+    boost::filesystem::path outdir(CMAKE_CURRENT_BINARY_DIR "/data/");
     if (!exists(outdir)) {
         BOOST_REQUIRE(create_directory(outdir));
     }
 
     for(boost::filesystem::directory_entry & x : boost::filesystem::directory_iterator(indir))
     {
-        //std::string eventFile = "AfdxFrame.blf";
+        //std::string eventFile = "CANSystem_A.blf";
         std::string eventFile = x.path().filename().string();
         std::cout << eventFile << std::endl;
 
         /* open input file */
         Vector::BLF::File filein;
-        boost::filesystem::path infile(CMAKE_CURRENT_SOURCE_DIR "/events/" + eventFile);
+        boost::filesystem::path infile(CMAKE_CURRENT_SOURCE_DIR "/data/" + eventFile);
         filein.open(infile.string(), Vector::BLF::File::OpenMode::Read);
         BOOST_REQUIRE(filein.is_open());
 
         /* open output file */
         Vector::BLF::File fileout;
-        boost::filesystem::path outfile(CMAKE_CURRENT_BINARY_DIR "/events/" + eventFile);
+        boost::filesystem::path outfile(CMAKE_CURRENT_BINARY_DIR "/data/" + eventFile);
         fileout.open(outfile.string(), Vector::BLF::File::OpenMode::Write);
         BOOST_REQUIRE(fileout.is_open());
+
+        /* check filein statistics */
+        BOOST_REQUIRE(filein.fileStatistics.signature == Vector::BLF::FileSignature);
 
         /* copy some filein to fileout statistics */
         fileout.fileStatistics.applicationId = filein.fileStatistics.applicationId;
@@ -49,16 +53,14 @@ BOOST_AUTO_TEST_CASE(AllEvents)
         fileout.fileStatistics.apiMinor = filein.fileStatistics.apiMinor;
         fileout.fileStatistics.apiBuild = filein.fileStatistics.apiBuild;
         fileout.fileStatistics.apiPatch = filein.fileStatistics.apiPatch;
-        fileout.fileStatistics.objectsRead = filein.fileStatistics.objectsRead; // @todo still don't know what this is...
         // all others should be set on close
 
-        /* parse log file */
         while(!filein.eof()) {
             Vector::BLF::ObjectHeaderBase * ohb;
             ohb = filein.read();
             BOOST_CHECK((ohb != nullptr) || filein.eof());
             if (ohb != nullptr) {
-                fileout.write(ohb);
+                // fileout.write(ohb);
                 delete ohb;
             } else {
                 break;
