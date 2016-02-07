f3ad2023abc40316a63881b7005b0fb3c132cf0c
Tobias Lorenz <tobias.lorenz@gmx.net>
2016-02-07 11:51:32 +0100

    FindZLIB: Search path for windows corrected.
    Signed-off-by: Tobias Lorenz <tobias.lorenz@gmx.net>

--- cmake/modules/FindZLIB.cmake
+++ cmake/modules/FindZLIB.cmake
@@ -2,14 +2,12 @@ find_path(ZLIB_INCLUDE_DIR
   NAMES zlib.h
   HINTS
     "$ENV{ProgramFiles}/zlib/include"
-    "[HKEY_LOCAL_MACHINE\\SOFTWARE\\GnuWin32\\Zlib;InstallPath]/include"
   DOC "zlib (http://www.zlib.net.net)")
 
 find_library(ZLIB_LIBRARY
-  NAMES z zlib zlibd
+  NAMES z zdll
   HINTS
     "$ENV{ProgramFiles}/zlib/lib"
-    "[HKEY_LOCAL_MACHINE\\SOFTWARE\\GnuWin32\\Zlib;InstallPath]/lib"
   DOC "zlib (http://www.zlib.net.net)")
 
 mark_as_advanced(ZLIB_LIBRARY ZLIB_INCLUDE_DIR)
