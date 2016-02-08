616557b9d36054cff155a14452e641e9abf833db
Tobias Lorenz <tobias.lorenz@gmx.net>
2016-02-09 00:27:27 +0100

    Add Doxygen main page.
    Signed-off-by: Tobias Lorenz <tobias.lorenz@gmx.net>

---
+++ src/Vector/BLF/Doxygen.h
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2013 Tobias Lorenz.
+ * Contact: tobias.lorenz@gmx.net
+ *
+ * This file is part of Tobias Lorenz's Toolkit.
+ *
+ * Commercial License Usage
+ * Licensees holding valid commercial licenses may use this file in
+ * accordance with the commercial license agreement provided with the
+ * Software or, alternatively, in accordance with the terms contained in
+ * a written agreement between you and Tobias Lorenz.
+ *
+ * GNU General Public License 3.0 Usage
+ * Alternatively, this file may be used under the terms of the GNU
+ * General Public License version 3.0 as published by the Free Software
+ * Foundation and appearing in the file LICENSE.GPL included in the
+ * packaging of this file.  Please review the following information to
+ * ensure the GNU General Public License version 3.0 requirements will be
+ * met: http://www.gnu.org/copyleft/gpl.html.
+ */
+
+/**
+ * @mainpage
+ *
+ * <h1>Copyright</h1>
+ * Copyright (C) 2013 Tobias Lorenz.<br>
+ * Contact: <a href="mailto:tobias.lorenz@gmx.net">tobias.lorenz@gmx.net</a>
+ *
+ * <h1>Commercial License Usage</h1>
+ * Licensees holding valid commercial licenses may use this file in
+ * accordance with the commercial license agreement provided with the
+ * Software or, alternatively, in accordance with the terms contained in
+ * a written agreement between you and Tobias Lorenz.
+ *
+ * <h1>GNU General Public License 3.0 Usage</h1>
+ * Alternatively, this file may be used under the terms of the GNU
+ * General Public License version 3.0 as published by the Free Software
+ * Foundation and appearing in the file LICENSE.GPL included in the
+ * packaging of this file.  Please review the following information to
+ * ensure the GNU General Public License version 3.0 requirements will be
+ * met: http://www.gnu.org/copyleft/gpl.html.
+ */
