a8e78c8c77b2073a028fe4b222f19bb97fd0b058
Tobias Lorenz <tobias.lorenz@gmx.net>
2016-02-15 22:32:16 +0100

    Harmonization of CMakeLists.txt. Use GNUInstallDirs.
    Signed-off-by: Tobias Lorenz <tobias.lorenz@gmx.net>

--- cmake/modules/FindZLIB.cmake
+++ cmake/modules/FindZLIB.cmake
@@ -1,4 +1,4 @@
-find_path(ZLIB_INCLUDE_DIR
+find_path(ZLIB_INCLUDE_DIRS
   NAMES zlib.h
   HINTS
     "$ENV{ProgramFiles}/zlib/include"
@@ -10,7 +10,7 @@ find_library(ZLIB_LIBRARY
     "$ENV{ProgramFiles}/zlib/lib"
   DOC "zlib (http://www.zlib.net.net)")
 
-mark_as_advanced(ZLIB_LIBRARY ZLIB_INCLUDE_DIR)
+mark_as_advanced(ZLIB_LIBRARY ZLIB_INCLUDE_DIRS)
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(ZLIB DEFAULT_MSG ZLIB_LIBRARY ZLIB_INCLUDE_DIR)
+find_package_handle_standard_args(ZLIB DEFAULT_MSG ZLIB_LIBRARY ZLIB_INCLUDE_DIRS)
