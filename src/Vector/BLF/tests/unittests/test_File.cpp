932a71a8309d59a50dc363d71969887d8a6f44de
Tobias Lorenz <tobias.lorenz@gmx.net>
2018-01-29 16:36:45 +0100

    Intermediate care for test failure...
    Signed-off-by: Tobias Lorenz <tobias.lorenz@gmx.net>

--- src/Vector/BLF/tests/unittests/test_File.cpp
+++ src/Vector/BLF/tests/unittests/test_File.cpp
@@ -20,7 +20,6 @@ BOOST_AUTO_TEST_CASE(OpenErrors)
     file.open(CMAKE_CURRENT_BINARY_DIR "/events_from_binlog/DirNotExists/File.blf", std::ios_base::out);
     BOOST_CHECK(!file.is_open());
 
-#if 0
     /* open an existing file */
     file.open(CMAKE_CURRENT_SOURCE_DIR "/events_from_binlog/test_CanMessage.blf", std::ios_base::in);
     BOOST_CHECK(file.is_open());
@@ -28,7 +27,9 @@ BOOST_AUTO_TEST_CASE(OpenErrors)
     /* open it again */
     file.open(CMAKE_CURRENT_SOURCE_DIR "/events_from_binlog/test_CanMessage.blf", std::ios_base::in);
     BOOST_CHECK(file.is_open());
-#endif
+
+    /* sleep for one seconds for the threads to settle */
+    sleep(1); // @todo this is to prevent  threads from hard termination.
 }
 
 /** try to create objects that are not triggered anyway by other test cases */
