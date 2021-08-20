22415ebbf6e55246fc0f54fd28d3b766ff1ff3a6
Ayoub Kaanich <ayoub.kaanich@technica-engineering.de>
2021-08-20 10:47:16 +0200

    Fix Reading EthernetStatus

--- src/Vector/BLF/EthernetStatus.cpp
+++ src/Vector/BLF/EthernetStatus.cpp
@@ -29,6 +29,7 @@ EthernetStatus::EthernetStatus() :
 }
 
 void EthernetStatus::read(AbstractFile & is) {
+    apiMajor = 1;
     ObjectHeader::read(is);
     is.read(reinterpret_cast<char *>(&channel), sizeof(channel));
     is.read(reinterpret_cast<char *>(&flags), sizeof(flags));
@@ -41,6 +42,13 @@ void EthernetStatus::read(AbstractFile & is) {
     is.read(reinterpret_cast<char *>(&pairs), sizeof(pairs));
     is.read(reinterpret_cast<char *>(&hardwareChannel), sizeof(hardwareChannel));
     is.read(reinterpret_cast<char *>(&bitrate), sizeof(bitrate));
+
+    /* the following variables are only available in Version 2 and above */
+    if (objectSize > calculateObjectSize()) {
+        apiMajor = 2;
+        is.read(reinterpret_cast<char *>(&reservedEthernetStatus1), sizeof(reservedEthernetStatus1));
+        is.read(reinterpret_cast<char *>(&reservedEthernetStatus2), sizeof(reservedEthernetStatus2));
+    }
 }
 
 void EthernetStatus::write(AbstractFile & os) {
@@ -56,6 +64,11 @@ void EthernetStatus::write(AbstractFile & os) {
     os.write(reinterpret_cast<char *>(&pairs), sizeof(pairs));
     os.write(reinterpret_cast<char *>(&hardwareChannel), sizeof(hardwareChannel));
     os.write(reinterpret_cast<char *>(&bitrate), sizeof(bitrate));
+
+    if (apiMajor < 2)
+        return;
+    os.write(reinterpret_cast<char *>(&reservedEthernetStatus1), sizeof(reservedEthernetStatus1));
+    os.write(reinterpret_cast<char *>(&reservedEthernetStatus2), sizeof(reservedEthernetStatus2));
 }
 
 uint32_t EthernetStatus::calculateObjectSize() const {
